8a327c033ed033cb881f48794aad4f30d7378d4b
Lim Jin Jhu <JinJhu.Lim@latticesemi.com>
2024-06-21 15:24:48 +0800

    Update CRC checking
    Update CRC checking to include different condition
    
    Signed-off-by: Lim Jin Jhu <JinJhu.Lim@latticesemi.com>

--- cmd/boot.c
+++ cmd/boot.c
@@ -28,35 +28,41 @@ static int do_go(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[])
 	u16     calculated_crc, read_crc;
 	u8      *addr = NULL;
 
+#if IS_ENABLED(CONFIG_CRC_CHECKING)
 	if (argc < 3)
 		return CMD_RET_USAGE;
 
 	addr = hextoul(argv[1], NULL);
 	length = hextoul(argv[2], NULL);
 
-#if IS_ENABLED(CONFIG_CRC_CHECKING)
 	printf ("## Checking CRC\n");
 	read_crc = (addr[length - 2] << 8)  | addr[length - 1];
 	calculated_crc = crc16_ccitt(0x1D0F, addr, length - 2);
 	if (read_crc != calculated_crc) {
 		printf("Bad firmware CRC: file 0x%04x calculated 0x%04x\n",
 			read_crc, calculated_crc);
+		goto done;
 	} else {
 		printf("CRC pass: 0x%04x\n", calculated_crc);
 	}
+#else
+	if (argc < 2)
+		return CMD_RET_USAGE;
+	
+	addr = hextoul(argv[1], NULL);
 #endif
-
 	printf ("## Starting application at 0x%08lX ...\n", addr);
 	flush();
 
 	/*
-	 * pass address parameter as argv[0] (aka command name),
-	 * and all remaining args
-	 */
+	* pass address parameter as argv[0] (aka command name),
+	* and all remaining args
+	*/
 	rc = do_go_exec ((void *)addr, argc - 1, argv + 1);
 	if (rc != 0) rcode = 1;
 
 	printf ("## Application terminated, rc = 0x%lX\n", rc);
+done:
 	return rcode;
 }
 
