046712e6584c5d83c89287c7963eef62f2db20fd
Kah Jing Lee <KahJing.Lee@latticesemi.com>
2024-10-02 15:05:15 +0800

    common: spl: spl_mtd: Cleanup on spl_mtd_load_image
    Cleanup on the spl_mtd_load_image to use spl_load function.
    
    Signed-off-by: Kah Jing Lee <KahJing.Lee@latticesemi.com>

--- common/spl/spl_mtd.c
+++ common/spl/spl_mtd.c
@@ -7,25 +7,21 @@
  * <www.latticesemi.com>
  */
 #include <common.h>
-#include <gzip.h>
 #include <image.h>
 #include <imx_container.h>
 #include <log.h>
-#include <mapmem.h>
 #include <mtd.h>
 #include <errno.h>
 #include <spl.h>
+#include <spl_load.h>
 #include <asm/global_data.h>
 #include <asm/io.h>
 #include <dm/ofnode.h>
-#include <asm/u-boot.h>
-#include <linux/libfdt.h>
 
 static struct mtd_info *get_mtd_dev_by_name(const char *name)
 {
 	struct mtd_info *mtd;
 
-	//mtd_probe_devices();
 	mtd_probe_devices();
 
 	mtd = get_mtd_device_nm(name);
@@ -58,20 +54,19 @@ static int spl_mtd_load_image(struct spl_image_info *spl_image,
 {
 	int err = 0;
 	unsigned int payload_offs, ram_offs;
-	struct legacy_img_hdr *header;
 	struct mtd_info *mtd;
 	unsigned int *buf;
-	size_t retlen;
+	struct spl_load_info load;
 
 	mtd = get_mtd_dev_by_name("lattice_qspi");
-	if (!mtd)
+	if (!mtd) {
+		debug("%s: Failed to get mtd device\n", __func__);
 		return -ENODEV;
+	}
 
 	/*
 	 * Load U-Boot image from Lattice QSPI flash into RAM
 	 */
-	header = spl_get_load_buffer(-sizeof(*header), sizeof(*header));
-
 	if (CONFIG_IS_ENABLED(OF_REAL)) {
 		payload_offs = (loff_t)ofnode_conf_read_int("u-boot,spl-payload-flash-offset",
 							    payload_offs);
@@ -80,57 +75,13 @@ static int spl_mtd_load_image(struct spl_image_info *spl_image,
 		buf = (unsigned int *)ram_offs;
 	}
 
-	/* Load u-boot, mkimage header is 64 bytes. */
-	err = mtd_read(mtd, (loff_t)payload_offs, (size_t)sizeof(*header),
-				    &retlen, (void *)buf);
-	if (err) {
+	load.priv = mtd;
+	spl_set_bl_len(&load, 1);
+	load.read = spl_mtd_fit_read;
+	err = spl_load(spl_image, bootdev, &load, 0, payload_offs);
+	if (err)
 		debug("%s: Failed to read from SPI flash (err=%d)\n",
 		      __func__, err);
-		return err;
-	}
-
-	if (IS_ENABLED(CONFIG_SPL_LOAD_FIT_FULL) &&
-	    image_get_magic(header) == FDT_MAGIC) {
-		u32 size = roundup(fdt_totalsize(header), 4);
-
-		err = mtd_read(mtd, (loff_t)payload_offs,
-					    (size_t)size, &retlen,
-					    map_sysmem(CONFIG_SYS_LOAD_ADDR,
-							size));
-		if (err)
-			return err;
-		err = spl_parse_image_header(spl_image, bootdev,
-					     phys_to_virt(CONFIG_SYS_LOAD_ADDR));
-		} else if (IS_ENABLED(CONFIG_SPL_LOAD_FIT) &&
-			   image_get_magic(header) == FDT_MAGIC) {
-			struct spl_load_info load;
-
-			puts("Found FIT\n");
-			load.priv = NULL;
-			spl_set_bl_len(&load, 1);
-			load.read = spl_mtd_fit_read;
-			err = spl_load_simple_fit(spl_image, &load,
-						  payload_offs,
-						  header);
-		} else if (IS_ENABLED(CONFIG_SPL_LOAD_IMX_CONTAINER) &&
-			   valid_container_hdr((void *)header)) {
-			struct spl_load_info load;
-
-			load.priv = NULL;
-			spl_set_bl_len(&load, 1);
-			load.read = spl_mtd_fit_read;
-
-			err = spl_load_imx_container(spl_image, &load,
-						     payload_offs);
-		} else {
-			err = spl_parse_image_header(spl_image, bootdev, header);
-			if (err)
-				return err;
-			err = mtd_read(mtd, (loff_t)payload_offs + spl_image->offset,
-					     (size_t)spl_image->size, &retlen,
-					     map_sysmem(spl_image->load_addr,
-							spl_image->size));
-		}
 
 	return err;
 }
